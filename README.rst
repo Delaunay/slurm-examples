8d5017fd8927bf6d4046ac058ede6121f9b70b71
Setepenre <pierre.delaunay@outlook.com>
2021-12-06 17:57:24 -0500

    add example python package

---
+++ README.rst
@@ -0,0 +1,12 @@
+seedproject
+=============================
+
+
+.. code-block:: bash
+
+   pip install seedproject
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(
+        name='seedproject',
+        version='0.0.0',
+        description='seeddescription',
+        author='seedauthor',
+        packages=[
+            'seedproject',
+        ],
+        setup_requires=['setuptools'],
+    )

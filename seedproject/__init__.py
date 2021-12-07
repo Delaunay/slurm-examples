9c480a6898e32d4258a347d2876bf8d6a5c4255a
Setepenre <pierre.delaunay@outlook.com>
2021-12-06 19:06:58 -0500

    Remove example

--- seedproject/__init__.py
+++ seedproject/__init__.py
@@ -1,3 +1,5 @@
+"""Seed module doc"""
+
 __descr__ = "seeddescription"
 __version__ = "seedversion"
 __license__ = "seedlicense"
@@ -5,35 +7,3 @@ __author__ = u"seedauthor"
 __author_email__ = "seed@email"
 __copyright__ = u"seedcopyright seedauthor"
 __url__ = "seedurl"
-
-
-def my_function(a: int, b: int) -> int:
-    """Add two numbers together
-
-    Parameters
-    ----------
-    a: int
-        first integer
-
-    b: int
-        second integer
-
-    Raises
-    ------
-    value errror if a == 0
-
-    Examples
-    --------
-
-    >>> my_function(1, 2)
-    3
-    >>> my_function(0, 2)
-    Traceback (most recent call last):
-      ...
-    ValueError
-
-    """
-    if a == 0:
-        raise ValueError()
-
-    return a + b

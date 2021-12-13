cd631e66d6586354def356bc7d65634d0c56a840
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-12-13 16:58:56 -0500

    -

--- scripts/single-gpu.sh
+++ scripts/single-gpu.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+
 # Usage:
 #   sbatch --gres=gpu:1 --cpus-per-gpu=4 --mem=16G scripts/single-gpu.sh seedproject/train_normal.py
 #
 
+# Slurm configuration
+# ===================
 #SBATCH --exclude=kepler4,kepler3
 
-# Setup our rendez-vous point
-RDV_ADDR=localhost
-WORLD_SIZE=$SLURM_JOB_NUM_NODES
-# -----
 
-module load miniconda/3
-conda activate py39
+# Setup
+# ===================
+
+module load python/3.7
+module load python/3.7/cuda/11.1/cudnn/8.0/pytorch
+source ~/envs/py37/bin/activate
 
 export SEEDPROJECT_DATASET_PATH=$SLURM_TMPDIR/dataset
 export SEEDPROJECT_CHECKPOINT_PATH=~/scratch/checkpoint

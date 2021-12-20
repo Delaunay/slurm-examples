6dd4d6b82f73a982c5f85fdc7f23651c3d0457b7
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-12-20 12:49:17 -0500

    -

--- scripts/setup_conda.sh
+++ scripts/setup_conda.sh
@@ -8,6 +8,6 @@ conda remove --name py39 --all -y
 conda create -n py39 python=3.9 -y
 conda activate py39
 
-conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch -y
 
 python -c "import torch; print('is cuda available: ', torch.cuda.is_available())"

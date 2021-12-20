f60082cf31bcb4195d073d72369e7212edfc5c17
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-12-20 13:04:25 -0500

    fix venv

--- scripts/setup_virtualenv.sh
+++ scripts/setup_virtualenv.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --exclude=kepler4,kepler3
 
-module load python/3.9
-virtualenv $HOME/py39
+module load python/3.8
+virtualenv $HOME/py38
 
-source $HOME/py39/bin/activate
+source $HOME/py38/bin/activate
 pip install torch torchvision
 
 python -c "import torch; print('is cuda available: ', torch.cuda.is_available())"

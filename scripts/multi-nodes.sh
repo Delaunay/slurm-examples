5c2842ce60b8d23e7fce6a217a9f00d93998dc9e
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-12-20 11:46:28 -0500

    revert env change

--- scripts/multi-nodes.sh
+++ scripts/multi-nodes.sh
@@ -28,8 +28,9 @@ WORLD_SIZE=$SLURM_JOB_NUM_NODES
 # Setup
 # ===================
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source $SCRIPT_DIR/env.sh
+module load python/3.7
+module load python/3.7/cuda/11.1/cudnn/8.0/pytorch
+source ~/envs/py37/bin/activate
 
 export SEEDPROJECT_DATASET_PATH=$SLURM_TMPDIR/dataset
 export SEEDPROJECT_CHECKPOINT_PATH=~/scratch/checkpoint

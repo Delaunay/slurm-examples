4a15db63341b67ffdafb14c5b7c72dded34bd1b9
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2022-01-18 10:55:37 -0500

    -

--- seedproject/train.py
+++ seedproject/train.py
@@ -7,7 +7,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 
--- seedproject/train_distributed.py
+++ seedproject/train_distributed.py
@@ -8,7 +8,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.distributed as dist
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 
